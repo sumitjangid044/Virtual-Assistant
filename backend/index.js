506dd9cc46e23c15836f5016839ffd2f5112dc56
Sumit Jangid <165179077+sumitjangid044@users.noreply.github.com>
2026-04-24 15:42:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import chatgptResponse from "./chatgpt.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtual-assistant-qz5f.onrender.com",
     credentials:true
 }))
 const port = process.env.PORT || 5000
